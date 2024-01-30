dd054cfd944f9d8d5c418d16586a6844a87874da
Aaron Espana Rubio <aaronrubio812@gmail.com>
2024-01-30 01:11:00 +0000

    Merge (or rebase) changes from origin/main

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from .views import send_message
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('send-message/', send_message, name='send_message'),
 ]
